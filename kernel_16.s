c2706de402a5973f73f672632f0d720c65d1599a
Florin Saftoiu <florin.saftoiu@soft-to-you.com>
2019-09-19 16:57:30 +0300

    fix comments for old 16bit kernel

--- kernel_16.s
+++ kernel_16.s
@@ -1,7 +1,7 @@
-# as kernel.s -o kernel.o
-# ld -T NUL -Ttext=0x8000 -o kernel.tmp kernel.o
-# objcopy -O binary -j .text kernel.tmp kernel.bin
-# dd if=kernel.bin of=drive.img seek=1 bs=512
+# as kernel_16.s -o kernel_16.o
+# ld -T NUL -Ttext=0x8000 -o kernel_16.tmp kernel_16.o
+# objcopy -O binary -j .text kernel_16.tmp kernel_16.bin
+# dd if=kernel_16.bin of=drive.img seek=1 bs=512
 .code16
 
 _kstart:
