916a1d232774cfa5a4aff005ffebb72121a0fe9d
Florin Saftoiu <florin.saftoiu@soft-to-you.com>
2020-05-06 02:11:07 +0300

    refactor bootsect using the stack to pass params

--- src/bootsect.s
+++ src/bootsect.s
@@ -10,146 +10,246 @@
 # continue
 # dd if=/dev/zero of=drive.img bs=512 count=2
 # dd if=bootsect.bin of=drive.img conv=notrunc
-.code16
-.set stage2_start, 0x8000
+    .code16
+
+    .set stage2_offset, 0x8000
+
+_0:
+    ljmp    $0x0, $_start
+
 _start:
     cli
+
     xor     %ax, %ax
-    mov     %ax, %ds                        # useful for call to _print
-    mov     %ax, %es                        # useful for call to _readsec
-    mov     $((stage2_start - 0x200) / 0x10), %ax
+    mov     %ax, %ds
+    mov     %ax, %es
+    mov     $((stage2_offset - 0x200) / 0x10), %ax
     mov     %ax, %ss                        # stack starts right after the boot sector
-    mov     $0x1fe, %sp                     # stack length 512 bytes, will grow down from stage2_start
+    mov     $0x200, %sp                     # stack length 512 bytes, will grow down from stage2_start
+
     sti
-    mov     $msg_start, %si
+
+    mov     %sp, %bp                        # use %bp to point at the current stack top
+    sub     $6, %sp                         # make room for 2 local variables, 1 uint16_t and 1 params (see _params)
+
+    xor     %dh, %dh                        # make sure high byte of %dx is 0, because we can't just push %dl
+    mov     %dx, -2(%bp)                    # save drive number left by bios in %dl in 1st local variable
+
+    push    $msg_start
     call    _print
+    add     $2, %sp
 
-    xor     %dh, %dh
-    push    %dx                             # save drive number left by BIOS in %dl
-    mov     $0x8, %ah
-    xor     %di, %di
-    int     $0x13
-    inc     %dh                             # maximum head number = %dh, number of heads = %dh + 1
-    mov     %dh, %dl
-    xor     %dh, %dh
-    mov     %dx, num_heads
-    and     $0b00111111, %cl                # remove bits 8..9 of maximum cylinder number
-    xor     %ch, %ch
-    mov     %cx, secs_per_track             # maximum sector number = number of sectors per track = bits 0..5 of %cl
-    pop     %dx                             # restore drive number left by BIOS in %dl into %dx
-    push    %dx                             # save drive number left by BIOS in %dl
+    lea     -6(%bp), %ax                    # load address of buffer for params into %ax
+    push    %ax                             # push address of buffer for return value
+    push    -2(%bp)                         # push drive number from 1st local variable for call to readsec
+    call    _params
+    add     $4, %sp                         # cleanup stack after return from params
 
-    mov     $3, %ch                         # load 3 sector(s)
-    mov     %dl, %cl                        # from drive left by BIOS in %dl
-    xor     %dx, %dx
-    mov     $1, %ax                         # starting at sector 1 (%dx:%ax)
-    mov     $stage2_start, %bx              # at stage2_start (%es:%bx)
+    test    %ah, %ah                        # if params returned status 0 then
+    jz      _load_stage2                    # load stage2
+
+    push    $msg_error                      # else, push error message as param for call to print
+    call    _print
+    add     $2, %sp                         # cleanup stack after return from print
+    jmp     _hang
+
+_load_stage2:
+    push    $stage2_offset                  # load stage2 at stage2_offset
+    push    $3                              # read 3 sectors
+    push    $1                              # low bytes of starting sector 1
+    push    $0                              # high bytes of starting sector 1
+    lea     -6(%bp), %ax                    # load address of params into %ax
+    push    %ax                             # push address of params
+    push    -2(%bp)                         # push drive number from 1st local variable
     call    _readsec
-    jnc     _read_pt
+    add     $12, %sp                        # cleanup stack after return from readsec
 
-    mov     $msg_error, %si                 # if error print message and hang
+    test    %ah, %ah                        # if readsec returned status 0 then
+    jz      _read_pt                        # read partition table
+
+    push    $msg_error                      # else, push error message as param for call to print
     call    _print
+    add     $2, %sp                         # cleanup stack after return from print
     jmp     _hang
 
 _read_pt:
-    mov     $msg_ok, %si
+    push    $msg_ok
     call    _print
+    add     $2, %sp
 
     mov     $pt, %di
     mov     $4, %cx
 1:
     movb    (%di), %al
-    test    $0x80, %al
-    jnz     _stage2
+    test    $0x80, %al                      # if active partition found, then
+    jnz     _stage2                         # call stage2
     add     $16, %di
     loop    1b
-    mov     $msg_pt_error, %si              # no active partition found
+
+    push    $msg_pt_error                   # no active partition found
     call    _print
+    add     $2, %sp
     jmp     _hang
 
 _stage2:
-    pop     %cx                             # restore drive number left by BIOS in %dl into %cx
+    mov     -2(%bp), %cx                    # drive number into %cx
     movw    8(%di), %ax
     movw    10(%di), %dx                    # %dx:%ax = starting sector of active partition in LBA format
 
-    mov     $stage2_start, %bx
+    mov     $stage2_offset, %bx
     jmp     *%bx
 
 _hang:
     jmp     _hang
 
 # print a null terminated string
-# input:  %ds:%si - string address
+# input: string address
 # output: none
+# void _print(char* str)
 _print:
-    lodsb
-    or      %al, %al
-    jz      1f
-    mov     $0xe, %ah
-    mov     $0x7, %bx
-    int     $0x10
-    jmp     _print
-1:
+    push    %bp                             # save caller's %bp
+    mov     %sp, %bp                        # use %bp to point at the current stack top
+
+    push    %bx                             # save %bx
+    push    %si                             # save %si
+
+    mov     $0xe, %ah                       # teletype output function code for int 0x10
+    mov     $0x7, %bx                       # output on page 0 (0 in %bh) with white on black (0 << 4 + 7 in %bl)
+
+    mov     4(%bp), %si                     # load string address into %si from parameter, right above caller's %bp and return address
+1:                                          # loop through the string at %ds:%si
+    lodsb                                   # loading each byte into %al
+    or      %al, %al                        # if current byte is null
+    jz      2f                              # exit loop
+    int     $0x10                           # call int 0x10
+    jmp     1b                              # loop around
+2:
+
+    pop     %si                             # restore %si
+    pop     %bx                             # restore %bx
+
+    mov     %bp, %sp                        # restore %sp
+    pop     %bp                             # restore caller's %bp
     ret
 
-# read %ch sectors from disk %cl starting at sector %dx:%ax (in LBA format)
-# into buffer starting at %es:%bx
-# input:  %ch - number of sectors to read
-#         %cl - drive number
-#         %dx:%ax - logical sector number of the start sector
-#         %es:%bx - buffer address
-# output: data from logical sector in %ds:%ax at memory location %es:%bx
-#         CF - set on error
-_readsec:
-    mov     $3, %di                         # retry counter
-    push    %cx                             # put number of sectors to read and drive number on stack
-    mov     %sp, %bp                        # save a stack pointer in %bp
-1:
-    divw    secs_per_track
-    mov     %dl, %cl
-    inc     %cl                             # %cl = physical sector = LBA sector % sec_per_track + 1
+# read drive params
+# input: drive - drive number
+# output: return value - structure with status, number of cylinders, number of heads, number of sectors
+# typedef struct {
+#     uint16_t cylinders;
+#     uint8_t heads;
+#     uint8_t sectors;
+# } params;
+# uint16_t _params(uint16_t drive, params* params)
+_params:
+    push    %bp                             # save caller's %bp
+    mov     %sp, %bp                        # use %bp to point at the current stack top
 
-    xor     %dx, %dx
-    divw    num_heads
-    mov     %al, %ch                        # %ch = bits 0..7 of cylinder = LBA sector / (num_heads * sec_per_track) = (LBA sector / sec_per_track) / num_heads
-    shl     $6, %ah
-    add     %ah, %cl                        # %cl = bits 8..9 of cylinder + physical sector
+    push    %di                             # save %di
+    push    %bx                             # save %bx, because int 0x13, ah 0x8 trashes it
 
-    mov     %dl, %dh                        # %dh = head = (LBA sector / sec_per_track) % num_heads
+    mov     $0x8, %ah                       # get drive parameters function code for int 0x13
+    mov     4(%bp), %dl                     # load drive number into %dl from parameter
+    xor     %di, %di                        # %es:%di should be 0x0:0x0 for call to int 0x13 get drive params
+    int     $0x13                           # call int 0x13, status is put in %ax and returned as is
+
+    mov     6(%bp), %bx                     # load address of buffer from parameter
+    mov     %ch, %ss:(%bx)                  # put low bits of max cylinder into low bits of params->cylinders
+    mov     %cl, %ss:1(%bx)                 # isolate high bits of max cylinder
+    shrb    $6, %ss:1(%bx)                  # into high bits of params->cylinders
+    incw    %ss:(%bx)                       # get number of cylinders into params->cylinders
+    mov     %dh, %ss:2(%bx)                 # put max head into params->heads
+    incb    %ss:2(%bx)                      # get number of heads into params->heads
     
-    movb    (%bp), %dl                      # %dl = drive number
-    movb    1(%bp), %al                     # %al = number of sectors to read
+    mov     %cl, %ss:3(%bx)                 # put max sector into params->sector
+    andb    $0b00111111, %ss:3(%bx)         # isolate max sector, same as number of sectors, since it starts at 1
 
-    mov     $0x2, %ah
-    int     $0x13
-    jnc     2f                              # all went well, return
+    pop     %bx                             # restore %bx
+    pop     %di                             # restore %di
+
+    mov     %bp, %sp                        # restore %sp
+    pop     %bp                             # restore caller's %bp
+    ret
+
+# read sectors from a disk starting at a given sector (in LBA format) into a buffer
+# input: drive - drive number
+#        params - drive params
+#        start - logical sector number of the start sector
+#        nb - number of sectors to read
+#        buffer - buffer address
+# output: data from logical sector in start at memory location in buffer
+#         high byte of return value - status, 0 if no error
+#         low byte of return value - number of sectors actually read
+# uint16_t _readsec(uint16_t drive, params* params, uint32_t start, uint16_t nb, void* buffer)
+_readsec:
+    push    %bp                             # save caller's %bp
+    mov     %sp, %bp                        # use %bp to point at the current stack top
+
+    sub     $6, %sp                         # make room for 3 local variable
+
+    push    %bx                             # save %bx
+    push    %di                             # save %di
+
+    mov     6(%bp), %bx                     # load address of params from parameter
+    movzbw  %ss:2(%bx), %ax                 # zero-expand params->heads into %ax
+    mov     %cx, -2(%bp)                    # move zero-expanded params->heads into -2(%bp)
+    movzbw  %ss:3(%bx), %ax                 # zero-expand params->sectors into %ax
+    mov     %cx, -4(%bp)                    # move zero-expanded params->sectors into -4(%bp)
+    xor     %dx, %dx
+    mov     8(%bp), %ax                     # load high bytes of start into %ax from parameter
+    divw    -4(%bp)                         # divide by params->sectors
+    mov     %ax, -6(%bp)                    # save high bytes of quotient in 3rd local variable
+    mov     10(%bp), %ax                    # load low bytes of start into %ax from parameter
+    divw    -4(%bp)                         # divide by params->sectors
+    mov     %dl, %cl                        # %cl = LBA sector % secs_per_track
+    inc     %cl                             # %cl = physical sector = LBA sector % secs_per_track + 1
+
+    mov     -6(%bp), %dx                    # load saved high bytes of quotient from 3rd local variable
+    divw    -2(%bp)                         # divide by params->heads
+    mov     %al, %ch                        # %ch = bits 0..7 of cylinder = LBA sector / (num_heads * secs_per_track) = (LBA sector / secs_per_track) / num_heads
+    shl     $6, %ah
+    or      %ah, %cl                        # %cl = bits 8..9 of cylinder | physical sector
+
+    mov     %dl, %dh                        # %dh = head = (LBA sector / secs_per_track) % num_heads
+    
+    movb    4(%bp), %dl                     # load drive number into %dl from parameter
+    movb    12(%bp), %al                    # load number of sectors to read into %al from paramter
+
+    mov     14(%bp), %bx                    # load address of buffer into %bx from parameter
+
+    mov     $3, %di                         # retry counter
+1:
+    mov     $0x2, %ah                       # read sectors function code for int 0x13
+    int     $0x13                           # call int 0x13
+    jnc     2f                              # all went well, return status 0 in %ah and nb of sectors in %al
 
     dec     %di
-    jz      2f                              # if retry count is 0 give up
-    
-    mov     $0x0, %ah                       # if error, reset disk system and try again
-    int     $0x13
-    jnc     1b
+    jz      2f                              # if retry count is 0 give up, return status != 0 in %ah and nb of sectors in %al
+
+    mov     $0x0, %ah                       # if error, reset disk system function code for int 0x13
+    int     $0x13                           # call int 0x13
+    jnc     1b                              # try again
 2:
-    pop     %cx
+
+    pop     %di                             # restore %di
+    pop     %bx                             # restore %bx
+
+    mov     %bp, %sp                        # restore %sp
+    pop     %bp                             # restore caller's %bp
     ret
 
 # data
 msg_start:
-    .asciz "Loading 2nd stage..."
+    .asciz "OSDEV\r\nLoading 2nd stage..."
 msg_ok:
     .asciz " OK."
 msg_error:
     .asciz " failed."
 msg_pt_error:
     .asciz "\r\nError reading partition table."
-secs_per_track:
-    .word 63
-num_heads:
-    .word 16
 
 # fill up the sector
-.fill 440 - (. - _start), 1, 0
+    .fill 440 - (. - _0), 1, 0
 
 unique_disk_id:
     .long 0xf41aa958
@@ -173,4 +273,4 @@ pt:
     .fill 16, 1, 0                          # partition 4
 
 # boot identifier
-.word 0xaa55
+    .word 0xaa55
